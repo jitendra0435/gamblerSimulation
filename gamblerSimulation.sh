91f625184f9b6fe0fdba5a2bad9c33e9880ad22e
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-14 12:16:23 +0530

    Add condtion for playing,if player win fifty percent amount of stakeamount or loose should stop playing

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,19 +1,34 @@
 #!/bin/bash -x
 
-   StakeValueperDay=100
+   stakeValueperDay=100
    betAmountperGame=1
+   MINAMOUNTFORPLAY=$(( $stakeValueperDay/ 2))
+   MAXAMOUNTFORPLAY=$(( $stakeValueperDay+$MINAMOUNTFORPLAY))
    win=0
    loose=0
-
   function bet()
   {
     checkWinOrloose=$((RANDOM%2))
     if [ $checkWinOrloose -eq 1 ]
     then
     win=$(( $win+1 ))
+    stakeValueperDay=$(( $stakeValueperDay+1 ))
     else
     loose=$(( $loose+1))
+    stakeValueperDay=$(( $stakeValueperDay-1 ))
     fi
  }
 
- bet
+ function gambler
+ {
+  while [ $stakeValueperDay -lt  $MAXAMOUNTFORPLAY  ] && [ $stakeValueperDay -gt  $MINAMOUNTFORPLAY ]
+  do
+  bet
+  done
+ }
+
+gambler
+
+
+
+ 
