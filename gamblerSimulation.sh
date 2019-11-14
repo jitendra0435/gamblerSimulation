77bd777d12662ef16a35eb5797efb621a3e20b00
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-14 11:13:32 +0530

    Add feature for how many price he got after winning and how loose after lost

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -2,5 +2,18 @@
 
    StakeValueperDay=100
    betAmountperGame=1
-   
-  
+   win=0
+   loose=0
+
+  function bet()
+  {
+    checkWinOrloose=$((RANDOM%2))
+    if [ $checkWinOrloose -eq 1 ]
+    then
+    win=$(( $win+1 ))
+    else
+    loose=$(( $loose+1))
+    fi
+ }
+
+ bet
