634410cf905cb11c53eb3c79051fea10ff660689
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-14 16:19:00 +0530

    Add the feature for know the amount player lost or win

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -1,34 +1,64 @@
-#!/bin/bash -x
+  #!/bin/bash -x
 
-   stakeValueperDay=100
-   betAmountperGame=1
-   MINAMOUNTFORPLAY=$(( $stakeValueperDay/ 2))
-   MAXAMOUNTFORPLAY=$(( $stakeValueperDay+$MINAMOUNTFORPLAY))
+   STAKE=100 
+   totalAmount=0
+   betAmountpergame=1
+   numberOfdaysforplay=20
    win=0
    loose=0
-  function bet()
-  {
-    checkWinOrloose=$((RANDOM%2))
-    if [ $checkWinOrloose -eq 1 ]
-    then
-    win=$(( $win+1 ))
-    stakeValueperDay=$(( $stakeValueperDay+1 ))
-    else
-    loose=$(( $loose+1))
-    stakeValueperDay=$(( $stakeValueperDay-1 ))
-    fi
- }
+   stakeValueperDay=0
+   percent=50
+   MINAMOUNTFORPLAY=$(( $STAKE*$percent/100))
+   MAXAMOUNTFORPLAY=$(( $STAKE+$MINAMOUNTFORPLAY))
 
- function gambler
- {
-  while [ $stakeValueperDay -lt  $MAXAMOUNTFORPLAY  ] && [ $stakeValueperDay -gt  $MINAMOUNTFORPLAY ]
-  do
-  bet
-  done
- }
+   declare -A dayHistory
 
-gambler
+   function bet()
+   {
+
+     checkWinOrloose=$((RANDOM%2))
+     if [ $checkWinOrloose -eq 1 ]
+      then
+      win=$(( $win+1))
+      stakeValueperDay=$(( $stakeValueperDay+$betAmountpergame ))
+      else
+      loose=$(( $loose+1))
+      stakeValueperDay=$(( $stakeValueperDay-$betAmountpergame ))
+     fi
+   }
+
+    function gambler
+    {
+     stakeValueperDay=100
+     for ((day=1;day<=20;day++ ))
+      do 
+       while [ $stakeValueperDay -lt  $MAXAMOUNTFORPLAY  ] && [ $stakeValueperDay -gt  $MINAMOUNTFORPLAY ]
+        do
+        bet
+        dictionary
+      done
+     done
+   }
 
 
 
- 
+
+   function dictionary
+   {
+      if [ $stakeValueperDay -eq $MINAMOUNTFORPLAY ]
+       then
+
+        dayHistory[$day]=$(($stakeValuperDay-$STAKE))
+        totalAmount=$(($totalAmount-$MINAMOUNTFORPLAY))
+        else
+
+        dayHistory[$day]=$(($stakeValueperDay-$STAKE))
+        totalAmount=$(($totalAmount+$stakeValueperDay-$STAKE))
+      fi
+
+   }
+ gambler
+# echo "total amount win or loss " $totalAmount
+
+
+
