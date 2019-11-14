91de963290c934ef17194b9f44a5ebec6a85df4d
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-14 19:17:04 +0530

    add feature for to determining the number of loose and win per month

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -6,16 +6,16 @@
    numberOfdaysforplay=20
    win=0
    loose=0
-   stakeValueperDay=0
+   stakeValueperDay=100
    percent=50
+   numberofdayloose=0
+   numberofdaywin=0
    MINAMOUNTFORPLAY=$(( $STAKE*$percent/100))
    MAXAMOUNTFORPLAY=$(( $STAKE+$MINAMOUNTFORPLAY))
 
    declare -A dayHistory
-
    function bet()
    {
-
      checkWinOrloose=$((RANDOM%2))
      if [ $checkWinOrloose -eq 1 ]
       then
@@ -24,40 +24,40 @@
       else
       loose=$(( $loose+1))
       stakeValueperDay=$(( $stakeValueperDay-$betAmountpergame ))
+
      fi
    }
 
     function gambler
     {
-     stakeValueperDay=100
      for ((day=1;day<=20;day++ ))
-      do 
+      do
+       dictionary
+       stakeValueperDay=100 
        while [ $stakeValueperDay -lt  $MAXAMOUNTFORPLAY  ] && [ $stakeValueperDay -gt  $MINAMOUNTFORPLAY ]
         do
         bet
-        dictionary
       done
      done
    }
 
-
-
-
-   function dictionary
-   {
-      if [ $stakeValueperDay -eq $MINAMOUNTFORPLAY ]
-       then
-
-        dayHistory[$day]=$(($stakeValuperDay-$STAKE))
+  function dictionary
+  {
+       if [ $stakeValueperDay -eq $MINAMOUNTFORPLAY ]
+        then
+        numberofdaywin=$(($numberofdaywin+1))
+        dayHistory[$day]=$(($stakeValueperDay-$STAKE))
         totalAmount=$(($totalAmount-$MINAMOUNTFORPLAY))
-        else
 
+        else
+        numberofdayloose=$(($numberofdayloose+1))
         dayHistory[$day]=$(($stakeValueperDay-$STAKE))
         totalAmount=$(($totalAmount+$stakeValueperDay-$STAKE))
       fi
 
-   }
- gambler
+  }
 
-
-
+  gambler
+  echo ${dayHistory[@]}
+  echo "number of day looose" $numberofdayloose
+  echo "number of day win " $numberofdaywin
